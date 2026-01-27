09e4ba7d59d224f4ccbea3badd92290788d844af
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-27 18:10:39 +0000

    #19 Created the files for each entity in the tables

---
+++ Backend/src/main/java/teamproject/backend/Model/ItemGroup.java
@@ -0,0 +1,4 @@
+package teamproject.backend.Model;
+
+public class ItemGroup {
+}
---
+++ Backend/src/main/java/teamproject/backend/Model/MenuItem.java
@@ -0,0 +1,8 @@
+package teamproject.backend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class MenuItem {
