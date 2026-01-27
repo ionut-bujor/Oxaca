165a27fa1a2b940d3b60460a89770d2443331223
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-27 19:10:18 +0000

    #19 Created the entity field for menu item and made its attributes

--- Backend/src/main/java/teamproject/backend/Model/MenuItem.java
+++ Backend/src/main/java/teamproject/backend/Model/MenuItem.java
@@ -1,8 +1,38 @@
 package teamproject.backend.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
 
 @Entity
 public class MenuItem {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false, length = 30)
+  private String name;
+
+  @Column (nullable = false, length = 100)
+  private String description;
+
+  @Column (nullable = false)
+  private int quantity;
+
+  @Column (nullable = false, precision =10, scale =2)
+  private float price;
+
+  @Column (nullable = false)
+  private String imageURL;
+
+  @ManyToOne
+  @JoinColumn(name = "item_group_id")
+  private ItemGroup itemGroup;
+
+}
+
