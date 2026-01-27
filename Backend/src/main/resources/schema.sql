1566c17c1ead63d63f95d22b1987a87216643b02
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-27 17:54:57 +0000

    #21 Added the fields for each menu_item and changed the variable names to standard camel case

--- Backend/src/main/resources/schema.sql
+++ Backend/src/main/resources/schema.sql
@@ -1,17 +1,26 @@
-CREATE TABLE menuTYPE(
-  name VARCHAR(30) PRIMARY KEY
-)
+-- determines which menu is being shown (main, drinks, desserts, starters)
+CREATE TABLE menu_type (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE
+);
 
-CREATE TABLE itemGroup(
-  name VARCHAR(30) PRIMARY KEY,
-  menuTYPE_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY (menuTYPE_name) REFERENCES menuTYPE (name)
-)
+-- subcategories within a menu (chicken, dishes)
+CREATE TABLE item_group(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    menu_type_id INT NOT NULL,
+    FOREIGN KEY (menu_type_id) REFERENCES menu_type(id)
+);
 
-CREATE TABLE menuItem(
-  name VARCHAR(30) PRIMARY KEY,
-  itemDescription VARCHAR(100) NOT NULL
-  --THIS IS WHERE DIETARY FIELDS + CALORIES WOULD GO.
-  itemGroup_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY(itemGroup_name) REFERENCES itemGroup (name)
-)
+-- the actual items (prawns, carbonara, etc.)
+CREATE TABLE menu_item (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(30) NOT NULL,
+    item_description VARCHAR(100) NOT NULL,
+    item_quantity int NOT NULL,
+    item_price NUMERIC(10,2) NOT NULL,
+    item_picture_url TEXT NOT NULL,
+    item_group_id INT NOT NULL,
+    FOREIGN KEY (item_group_id) REFERENCES item_group(id)
+);
+
