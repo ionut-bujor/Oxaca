463adc8eaca2b4edeb680fe3a84794a1e9b98a9d
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-28 15:28:16 +0000

    #19 Added the getters and setters for these attributes

--- Backend/src/main/java/teamproject/backend/Model/MenuItem.java
+++ Backend/src/main/java/teamproject/backend/Model/MenuItem.java
@@ -105,5 +105,29 @@ public class MenuItem {
   public void setName(String name) {
     this.name = name;
   }
+
+  public int getCalories() {
+    return calories;
+  }
+
+  public void setCalories(int calories) {
+    this.calories = calories;
+  }
+
+  public String getAllergens() {
+    return allergens;
+  }
+
+  public void setAllergens(String allergens) {
+    this.allergens = allergens;
+  }
+
+  public String getTags() {
+    return tags;
+  }
+
+  public void setTags(String tags) {
+    this.tags = tags;
+  }
 }
 
