b3d0028d1faf9731dbfc5ab6d122c411bf111325
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-29 18:44:46 +0000

    #28 mocked the repositories using mockito for unit testing

--- Backend/src/test/java/teamproject/backend/ServiceMenuTest.java
+++ Backend/src/test/java/teamproject/backend/ServiceMenuTest.java
@@ -1,5 +1,31 @@
 package teamproject.backend;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import teamproject.backend.Repository.ItemGroupRepository;
+import teamproject.backend.Repository.MenuItemRepository;
+import teamproject.backend.Repository.MenuTypeRepository;
+import teamproject.backend.Service.ServiceMenu;
+
 public class ServiceMenuTest {
+  @Mock
+  private MenuItemRepository menuItemRepo;
+
+  @Mock
+  private ItemGroupRepository itemGroupRepo;
+
+  @Mock
+  private MenuTypeRepository menuTypeRepo;
+
+  @InjectMocks
+  private ServiceMenu serviceMenu;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
 
 }
