b8cb6e85a71ceb794ee181f1570efe13f2dcccba
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-01-30 09:55:23 +0000

    #24 created the menu controller

---
+++ Backend/src/main/java/teamproject/backend/Controller/MenuController.java
@@ -0,0 +1,29 @@
+package teamproject.backend.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import teamproject.backend.DTO.MenuItemDTO;
+import teamproject.backend.Service.ServiceMenu;
+
+@RestController
+@RequestMapping("/api/v1/menu")
+public class MenuController {
+  private final ServiceMenu serviceMenu;
+
+  //constructing the service class (handles the logic behind data handling)
+  public MenuController(ServiceMenu serviceMenu) {
+    this.serviceMenu = serviceMenu;
+  }
+
+  //sends the data in json format to specified endpoint
+  @GetMapping
+  public ResponseEntity<List<MenuItemDTO>> displayMenu() {
+    List<MenuItemDTO> menuItems = serviceMenu.fetchAllAvailableItems();
+    return ResponseEntity.ok(menuItems);
+  }
+}
