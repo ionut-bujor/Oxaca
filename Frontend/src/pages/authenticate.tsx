46c16f4869c4c8f0f80f2def88190c929bf3741c
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-01 19:28:11 +0000

    created directory for distinct pages and began work on the login page under pages/authenticate.tsx, will expand on this much more as it's borderline empty once i get more information that I need this week

---
+++ Frontend/src/pages/authenticate.tsx
@@ -0,0 +1,10 @@
+export function authenticate() {
+	//unsure how to add a style to this specifically - all i know is this is how im suppoesd to layout my work for using a router to page in react?
+	return (
+		<div>
+			<button type="button">Continue as customer</button>
+			<br/><br/>
+			<button type="button">Login as staff</button>
+		</div>
+	)
+}
