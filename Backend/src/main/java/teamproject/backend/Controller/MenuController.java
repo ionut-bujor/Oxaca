bd6febeae51df3d86e9bdbcc4e5fd20a62385d84
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-02-02 12:04:48 +0000

    #24 changed the imports as file structure was outdated

--- Backend/src/main/java/teamproject/backend/Controller/MenuController.java
+++ Backend/src/main/java/teamproject/backend/Controller/MenuController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import teamproject.backend.DTO.MenuItemDTO;
-import teamproject.backend.Service.ServiceMenu;
+import teamproject.backend.dto.MenuItemDTO;
+import teamproject.backend.service.ServiceMenu;
 
 /**
  * Controller used to send data to the specified endpoint relating to menu items.
