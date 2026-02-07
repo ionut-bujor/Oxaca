e9d7aa6f7b6f7d0cf3bec743d0529d846541911d
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-07 13:15:55 +0000

    messing around with tailwind css to make login buttons look like those on the menu

--- Frontend/src/pages/auth.tsx
+++ Frontend/src/pages/auth.tsx
@@ -3,11 +3,15 @@ import React from "react";
 const Auth: React.FC = () => {
 	//unsure how to add a style to this specifically - all i know is this is how im suppoesd to layout my work for using a router to page in react?
 	return (
-		<div>
-			<button type="button">Continue as customer</button>
+		<div className="mt-8">
+			<button
+            	className="bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-accent transition-all shadow-xl shadow-primary/20 active:scale-95">
+                Continue as customer
+    		</button>
 			<br/><br/>
 			<button type="button">Login as staff</button>
 		</div>
+
 	)
 }
 
