0453b8f3bcdbe2715c3ea03e2ef0a7068772414e
Ahmed Omar Jaabir (2024) ZNAC277 <jaabir.ahmedomar.2024@live.rhul.ac.uk>
2026-02-08 22:57:36 +0000

    Fixed errors and created OrderController

---
+++ Backend/src/main/java/teamproject/backend/controller/OrderController.java
@@ -0,0 +1,52 @@
+package teamproject.backend.controller;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import teamproject.backend.dto.CustomerOrderDTO;
+import teamproject.backend.service.OrderService;
+
+/**
+ * This class handles POST requests.
+ */
+@RestController
+@RequestMapping("/v1")
+public class OrderController {
+
+  private final OrderService orderService;
+
+  /**
+   * This is the constructor for the controller.
+   *
+   * @param orderService This uses the Order Service class.
+   */
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+  /**
+  * This gets the order based on their id.
+  *
+  * @param id this is the given id.
+  * @return this returns the order in JSON.
+  */
+  @GetMapping("/orders/{id}")
+  public ResponseEntity<CustomerOrderDTO> getOrder(@PathVariable Long id) {
+    CustomerOrderDTO order = orderService.getOrderById(id);
+    return ResponseEntity.ok(order);
+  }
+
+  /**
+  * This gets all orders.
+  *
+  * @return all orders.
+  */
+  @GetMapping("/orders")
+  public ResponseEntity<List<CustomerOrderDTO>> getAllOrders() {
+    List<CustomerOrderDTO> orders = orderService.getAllOrders();
+    return ResponseEntity.ok(orders);
+  }
+}
