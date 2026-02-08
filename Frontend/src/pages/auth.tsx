67d88bbdfa4fef2e990d12b3c4a7aeea65981a5e
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-08 15:46:33 +0000

    centred option buttons on the portal page

--- Frontend/src/pages/auth.tsx
+++ Frontend/src/pages/auth.tsx
@@ -3,15 +3,24 @@ import React from "react";
 const Auth: React.FC = () => {
 	//unsure how to add a style to this specifically - all i know is this is how im suppoesd to layout my work for using a router to page in react?
 	return (
-		<div className="mt-8">
-			<button
-            	className="bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-accent transition-all shadow-xl shadow-primary/20 active:scale-95">
-                Continue as customer
-    		</button>
-			<br/><br/>
-			<button type="button">Login as staff</button>
+		<div className="min-h-screen selection:bg-primary/20 selection:text-primary">
+			<main>
+				<div style={{
+						minHeight: "100vh",
+						minWidth: "100vw",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center"
+					}}>
+					<button type="button">
+						Continue as customer
+					</button>
+					<br/><br/>
+					<button type="button">Login as staff</button>
+					<button type="button">test??</button>
+				</div>
+			</main>
 		</div>
-
 	)
 }
 
