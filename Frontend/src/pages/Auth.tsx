d6cc20431e9eddd5ba87f670f8ec979684e4f764
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-08 16:18:25 +0000

    added redirect from portal page to menu as a cusotmer

--- Frontend/src/pages/Auth.tsx
+++ Frontend/src/pages/Auth.tsx
@@ -9,7 +9,7 @@ const auth: React.FC = () => {
 				<div className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl">
 					<button 
 						className="bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
-						Continue as customer
+						<a href="/">Continue as customer</a>
 					</button>
 
 					<button 
