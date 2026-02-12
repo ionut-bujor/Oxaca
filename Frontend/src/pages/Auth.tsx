1d0b5b8b75fcda21694e687d26fd0a789271c961
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-12 18:23:11 +0000

    created request to login endpoint through the frontend #48

--- Frontend/src/pages/Auth.tsx
+++ Frontend/src/pages/Auth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { InputHTMLAttributes } from "react";
 import ButtonlessHeader from "../components/ButtonlessHeader";
 
 const auth: React.FC = () => {
@@ -10,10 +10,33 @@ const auth: React.FC = () => {
 				<div id="loginScreen" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl" style={{
 					display: "none"
 				}}>
-					<input type="text" placeholder="Username" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
-					<input type="password" placeholder="Password" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="text" id = "userField" placeholder="Username" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="password" id = "passField" placeholder="Password" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
 
-					<button className="w-full bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
+					<button className="w-full bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95"
+					onClick={() => {
+						let userField: HTMLInputElement = document.getElementById("userField") as HTMLInputElement
+						let passField: HTMLInputElement = document.getElementById("passField") as HTMLInputElement
+
+						if (!userField || !passField) {
+							return;
+						}
+
+						const headers: Headers = new Headers();
+						headers.set("Content-Type", "text/plain")
+
+						const request: RequestInfo = new Request("/api/v1/user/login", {
+							method: "POST",
+							headers: headers,
+
+							body: JSON.stringify({
+								email: userField.value,
+								password: passField.value
+							})
+						});
+
+						let response = fetch(request)
+					}}>
 					Login
 					</button>
 				</div>
