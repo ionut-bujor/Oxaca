8d15ea677b81a37989418cd2d2d4b68921717b48
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-16 16:49:45 +0000

    #52 Split UserController into two classes - one for managing users and the other for managing authentication

--- Backend/src/main/java/teamproject/backend/controller/UserController.java
+++ Backend/src/main/java/teamproject/backend/controller/UserController.java
@@ -1,19 +1,15 @@
 package teamproject.backend.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import teamproject.backend.dto.UserDTO;
-import teamproject.backend.model.User;
 import teamproject.backend.service.ServiceUser;
 
 /**
- * Controller used to send data to the specified endpoint relating to users.
+ * Controller for endpoints relating to user CRUD.
  */
 @RestController
 @RequestMapping("/api/v1/users")
@@ -29,58 +25,9 @@ public class UserController {
     this.serviceUser = serviceUser;
   }
 
-  /**
-   * API endpoint for login, parameters are passed from a form and authenticated against the user
-   * database. If successful, the user's session is stored.
-   *
-   * @param email - The parameter from the form with the provided email (parsed from the request)
-   * @param password - The parameter from the form with the provided password (parsed from the
-   *        request)
-   * @param request - The request itself.
-   * @return - HTTP 200 if user is successfully authenticated.
-   */
-  @PostMapping("/login")
-  public ResponseEntity<Void> login(@RequestParam String email, @RequestParam String password,
-      HttpServletRequest request) {
-
-    User user = serviceUser.authenticateUser(email, password);
-
-    HttpSession session = request.getSession(true);
-    serviceUser.storeSession(session, user);
-
-    return ResponseEntity.ok().build();
+  @GetMapping
+  public ResponseEntity<List<UserDTO>> displayUsers() {
+    List<UserDTO> allUsers = serviceUser.getAllUsers();
+    return ResponseEntity.ok(allUsers);
   }
-
-  /**
-   * API endpoint for logout, the request is received from the frontend and if a session is found,
-   * then it is invalidated and the user is "logged out".
-   *
-   * @param request - The request from the frontend
-   */
-  @PostMapping("/logout")
-  public void logout(HttpServletRequest request) {
-    HttpSession session = request.getSession(false);
-    if (session != null) {
-      session.invalidate();
-    }
-  }
-
-  /**
-   * API endpoint for getCurrentUser, finds the currently logged in user and returns it as a DTO.
-   *
-   * @param session - The session provided by Spring.
-   * @return - The current user in DTO format.
-   */
-  @GetMapping("/getCurrentUser")
-  public UserDTO getCurrentUser(HttpSession session) {
-    User user = serviceUser.getCurrentUser(session);
-
-    UserDTO userDto = new UserDTO();
-    userDto.setId(user.getId());
-    userDto.setEmail(user.getEmail());
-    userDto.setRole(user.getRole());
-
-    return userDto;
-  }
-
 }
