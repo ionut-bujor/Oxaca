2aa44eb3dbd50c2ef540a0e2e6afafa1760167d2
Hamza Hassan <ZNAC270@live.rhul.ac.uk>
2026-02-16 17:16:48 +0000

    Issue #22. Refactor Auth component structure and improve layout for login options

--- Frontend/src/pages/auth.tsx
+++ Frontend/src/pages/auth.tsx
@@ -1,27 +1,48 @@
 import React from "react";
+import ButtonlessHeader from "../components/ButtonlessHeader";
+import Login from "../components/Login";
 
-const Auth: React.FC = () => {
-	//unsure how to add a style to this specifically - all i know is this is how im suppoesd to layout my work for using a router to page in react?
+const auth: React.FC = () => {
 	return (
-		<div className="min-h-screen selection:bg-primary/20 selection:text-primary">
-			<main>
-				<div style={{
-						minHeight: "100vh",
-						minWidth: "100vw",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center"
-					}}>
-					<button type="button">
-						Continue as customer
+		<main>
+			<ButtonlessHeader />
+
+			<div className="flex items-center justify-center min-h-screen">
+				<div id="loginScreen" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl" style={{
+					display: "none"
+				}}>
+					<input type="text" placeholder="Username" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="password" placeholder="Password" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+
+					<button className="w-full bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
+					Login
 					</button>
-					<br/><br/>
-					<button type="button">Login as staff</button>
-					<button type="button">test??</button>
 				</div>
-			</main>
-		</div>
+
+				<div id="loginOptions" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl">
+					<button 
+						className="bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
+						<a href="/">Continue as customer</a>
+					</button>
+
+					<button 
+						className="bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95"
+						onClick={() => {
+							let loginOptions = document.getElementById("loginOptions");
+							let loginScreen = document.getElementById("loginScreen")
+							if (!loginOptions || !loginScreen) {
+								return;
+							}
+
+							loginOptions.style.display = "none";
+							loginScreen.style.display = "block"
+						}}>
+						Login as staff
+					</button>
+				</div>
+			</div>
+		</main>
 	)
 }
 
-export default Auth;
+export default auth;
