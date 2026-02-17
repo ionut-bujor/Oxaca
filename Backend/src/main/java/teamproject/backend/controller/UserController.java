4cb0f68a9723b0ff10c5a5bbeb0f9d7c6ed69149
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-17 11:40:53 +0000

    #54 Created new endpoint addUser in controller - need to implement service method now and update.

--- Backend/src/main/java/teamproject/backend/controller/UserController.java
+++ Backend/src/main/java/teamproject/backend/controller/UserController.java
@@ -2,10 +2,14 @@ package teamproject.backend.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import teamproject.backend.dto.UserDTO;
+import teamproject.backend.model.User;
 import teamproject.backend.service.ServiceUser;
 
 /**
@@ -15,6 +19,7 @@ import teamproject.backend.service.ServiceUser;
 @RequestMapping("/api/v1/users")
 public class UserController {
   private final ServiceUser serviceUser;
+  private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
   /**
    * Constructor used to inject the service class within the UserController.
@@ -25,9 +30,20 @@ public class UserController {
     this.serviceUser = serviceUser;
   }
 
-  @GetMapping
+  @GetMapping("/getAllUsers")
   public ResponseEntity<List<UserDTO>> displayUsers() {
     List<UserDTO> allUsers = serviceUser.getAllUsers();
     return ResponseEntity.ok(allUsers);
+  }
+
+  @PostMapping("/addUser")
+  public ResponseEntity<User> addUser(@RequestParam String email, @RequestParam String password) {
+    User user = new User();
+    user.setEmail(email);
+    user.setPasswordHash(passwordEncoder.encode(password));
+
+    // add in call to new method in serviceuser (addUser) that adds this user to the database.
+
+    return ResponseEntity.ok().build();
   }
 }
