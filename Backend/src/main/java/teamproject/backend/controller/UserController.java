23cfc081af7355a2e172d72bf573f23eb5e1d55d
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-17 13:29:09 +0000

    #54 Added javadoc to new endpoints.

--- Backend/src/main/java/teamproject/backend/controller/UserController.java
+++ Backend/src/main/java/teamproject/backend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpSession;
 import teamproject.backend.dto.UserDTO;
 import teamproject.backend.model.User;
 import teamproject.backend.service.ServiceUser;
@@ -30,19 +31,34 @@ public class UserController {
     this.serviceUser = serviceUser;
   }
 
+  /**
+   * API endpoint for getting all users currently stored in the DB - created initially to test
+   * frontend-backend communication.
+   *
+   * @return - The list of all users in the DB in DTO format.
+   */
   @GetMapping("/getAllUsers")
   public ResponseEntity<List<UserDTO>> displayUsers() {
     List<UserDTO> allUsers = serviceUser.getAllUsers();
     return ResponseEntity.ok(allUsers);
   }
 
+  /**
+   * API endpoint for adding a new user into the DB.
+   *
+   * @param email - The email of the new user.
+   * @param password - The password of the new user.
+   * @param session - The session provided by Spring.
+   * @return - HTTP OK if user is successfully added, else 400 BAD_REQUEST if an error occured.
+   */
   @PostMapping("/addUser")
-  public ResponseEntity<User> addUser(@RequestParam String email, @RequestParam String password) {
+  public ResponseEntity<User> addUser(@RequestParam String email, @RequestParam String password,
+      HttpSession session) {
     User user = new User();
     user.setEmail(email);
     user.setPasswordHash(passwordEncoder.encode(password));
 
-    // add in call to new method in serviceuser (addUser) that adds this user to the database.
+    serviceUser.addUser(user, session);
 
     return ResponseEntity.ok().build();
   }
