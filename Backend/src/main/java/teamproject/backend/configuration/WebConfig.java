9e58625b95cfad83d7cd71dd6fdc9555f208fdc4
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-19 17:37:52 +0000

    #60 Created WebConfig to register handler

---
+++ Backend/src/main/java/teamproject/backend/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package teamproject.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import teamproject.backend.security.RoleInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final RoleInterceptor roleInterceptor;
+
+  public WebConfig(RoleInterceptor roleInterceptor) {
+    this.roleInterceptor = roleInterceptor;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(roleInterceptor);
+  }
+}
+
