073363407f4cd81d647daa1d2449375556312c2b
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-19 17:32:36 +0000

    #60 Created require role annotation

---
+++ Backend/src/main/java/teamproject/backend/security/RequireRole.java
@@ -0,0 +1,13 @@
+package teamproject.backend.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import teamproject.backend.model.Role;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequireRole {
+  Role value();
+}
