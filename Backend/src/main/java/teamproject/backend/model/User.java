6ce5143e17fc6d480bac88047607256d48d1ed59
Zach Semmens <Zach.Semmens.2024@live.rhul.ac.uk>
2026-02-20 01:26:52 +0000

    all fixes needed to get menu working

--- Backend/src/main/java/teamproject/backend/model/User.java
+++ Backend/src/main/java/teamproject/backend/model/User.java
@@ -30,10 +30,10 @@ public class User {
   @Column(name = "role", nullable = false)
   private Role role;
 
-  @Column(name = "first_name", nullable = false)
+  @Column(name = "first_name", nullable = true)
   private String firstName;
 
-  @Column(name = "last_name", nullable = false)
+  @Column(name = "last_name", nullable = true)
   private String lastName;
 
   /**
--- Backend/src/main/resources/schema.sql
+++ Backend/src/main/resources/schema.sql
@@ -91,7 +91,7 @@ CREATE TABLE users (
                        email VARCHAR(255) NOT NULL UNIQUE,
                        password_hash VARCHAR(255) NOT NULL,
                        role VARCHAR(255) NOT NULL,
-                       first_name VARCHAR(255) NOT NULL,
-                       last_name VARCHAR(255) NOT NULL
+                       first_name VARCHAR(255),
+                       last_name VARCHAR(255)
 );
 
