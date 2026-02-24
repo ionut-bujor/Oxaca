1478112b36f18d70ab3de374d358f836c467328a
Bujor Ionut (2024) ZNAC600 <ionut.bujor.2024@live.rhul.ac.uk>
2026-02-24 15:38:12 +0000

    #66 working on the logic behind filtering orders based on logged in customers

--- Backend/src/main/java/teamproject/backend/controller/CustomerDashboardController.java
+++ Backend/src/main/java/teamproject/backend/controller/CustomerDashboardController.java
@@ -1,7 +1,13 @@
 package teamproject.backend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import teamproject.backend.dto.CustomerOrderDTO;
 import teamproject.backend.service.CustomerDashboardService;
 
 @RestController
@@ -11,5 +17,12 @@ public class CustomerDashboardController {
 
   public CustomerDashboardController(CustomerDashboardService cdService) {
     this.customerDashboardService= cdService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<CustomerOrderDTO>> displayCurrentOrders(HttpServletRequest request) {
+    HttpSession session = request.getSession(true);
+    List<CustomerOrderDTO> currentOrders = customerDashboardService.getCurrentOrders(session);
+    return ResponseEntity.ok(currentOrders);
   }
 }
--- Backend/src/main/java/teamproject/backend/service/CustomerDashboardService.java
+++ Backend/src/main/java/teamproject/backend/service/CustomerDashboardService.java
@@ -1,5 +1,12 @@
 package teamproject.backend.service;
 
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import teamproject.backend.dto.CustomerOrderDTO;
+import teamproject.backend.model.CustomerOrder;
+import teamproject.backend.model.User;
 import teamproject.backend.repository.CustomerOrderRepository;
 import teamproject.backend.repository.ItemGroupRepository;
 import teamproject.backend.repository.MenuItemRepository;
@@ -15,4 +22,25 @@ public class CustomerDashboardService {
     this.itemGroupRepo = itemGroupRepo;
     this.customerOrderRepo = customerOrderRepo;
   }
+
+  public List<CustomerOrderDTO> getCurrentOrders(HttpSession session) {
+    // pull the id of the user that is logged in to find the matching orders.
+    Long id = (Long) session.getAttribute("USER_ID");
+    List<CustomerOrder> matchingOrders = getOrdersWithID(id);
+
+    //filter orders here, not delivered and not paid.
+    //return those orders.
+  }
+
+public List<CustomerOrder> getOrdersWithID (Long id) {
+  List<CustomerOrder> allOrders = customerOrderRepo.findAllById(id) // thats orders wsith id i need users = id
+      .orElseThrow(() -> new ResponseStatusException(
+          HttpStatus.NOT_FOUND, "User doesnt have any orders"
+      ));
+  return allOrders;
+  }
+
 }
+
+
+
