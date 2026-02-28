ca1d23bc6c44b463d3969c393e8ec546a69796cf
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-28 00:54:57 +0000

    #82 created buttons and textfields to capture registration data (email, name, password)

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -3,7 +3,22 @@ import ButtonlessHeader from "../components/ButtonlessHeader";
 
 const register: React.FC = () => {
 	return (
-		<ButtonlessHeader />
+		<main>
+			<ButtonlessHeader />
+
+			<div className="flex items-center justify-center min-h-screen">
+				<div className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl">
+					<input type="text" placeholder="Email" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="text" placeholder="Full name" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="password" placeholder="Password" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+					<input type="password" placeholder="Confirm password" className="group flex flex-col gap-y-5 bg-white p-5 rounded-2xl border border-slate-100 shadow-2xl"/>
+
+					<button className="w-full bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
+						Register
+					</button>
+				</div>
+			</div>
+		</main>
 	)
 }
 
