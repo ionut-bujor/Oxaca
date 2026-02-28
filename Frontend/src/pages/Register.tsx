6f8679201a4ae7646ac5ffca9bc8cd15fca15c61
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-28 01:01:27 +0000

    #82 added error message tag

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -6,6 +6,7 @@ const register: React.FC = () => {
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
 	const [name, setName] = useState("");
+	const [error, setError] = useState("");
 
 	return (
 		<main>
@@ -29,6 +30,8 @@ const register: React.FC = () => {
 						value={confirmPassword}
 						onChange={(element) => setConfirmPassword(element.target.value)}
 					/>
+
+					{error && <p className="text-red-500">{error}</p>}
 
 					<button className="w-full bg-primary text-white px-8 py-4 rounded-xl font-bold uppercase tracking-widest hover:bg-darkGreen transition-all shadow-xl shadow-primary/20 active:scale-95">
 						Register
