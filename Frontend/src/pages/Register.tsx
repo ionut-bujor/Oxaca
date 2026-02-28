7442a7404f7555970638bdebf568a413cefd8a8b
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-02-28 00:39:22 +0000

    #82 created new page which shall contain registration

---
+++ Frontend/src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import ButtonlessHeader from "../components/ButtonlessHeader";
+
+const register: React.FC = () => {
+	return (
+		<ButtonlessHeader />
+	)
+}
+
+export default register
