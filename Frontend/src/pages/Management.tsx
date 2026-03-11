e85433ad4322b0837983ec651c38317caa8cc747
El-Zeenny Patrick (2024) ZNAC255 <patrick.el-zeenny.2024@live.rhul.ac.uk>
2026-03-11 18:29:59 +0000

    created and setup  new page file

---
+++ Frontend/src/pages/Management.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ButtonlessHeader from "../components/ButtonlessHeader";
+
+const management: React.FC = () => {
+	return (
+		<main>
+			<ButtonlessHeader />
+		</main>	
+	);
+}
+
+export default management;;
